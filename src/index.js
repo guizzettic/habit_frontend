b56e254d60129c20807f065fc8b4a839480b15a5
Chris Guizzetti <guizzettic@gmail.com>
2020-09-10 00:06:43 -0700

    used webpack to create an initial react app

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+// takes in two paramaters, 1: what we want to render, 2: where we want it to render
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'index_bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      // will look at this file as our template
+      template: './src/index.html',
+    }),
+  ],
+};
